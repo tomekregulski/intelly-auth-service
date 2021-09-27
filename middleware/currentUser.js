bbc0cdc71adb9a8c8884cbc9dcf1df8641b49cba
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-09-27 18:19:11 -0400

    add currentUser middleware

--- middleware/currentUser.js
+++ middleware/currentUser.js
@@ -2,13 +2,16 @@ const jwt = require('jsonwebtoken');
 
 const currentUser = (req, res, next) => {
   if (!req.session.jwt) {
-    return next();
+    // return next();
+    res.send({ currentUser: null });
   }
 
   try {
     const payload = jwt.verify(req.session.jwt, config.secret);
     req.currentUser = payload;
-  } catch (err) {}
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
   next();
 };
 
